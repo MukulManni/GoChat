0c7850bfeb462505bb614ce0ef78a04904cc3dde
Your Name <you@example.com>
2022-01-13 00:21:44 +0530

    commit main

--- static/js/script.js
+++ static/js/script.js
@@ -9,10 +9,17 @@ function repeat() {
     document.getElementById("globalmsgs").innerText = "";
     
     for (let i = 0; i < d.length; i++) {
-      document.getElementById("globalmsgs").innerHTML += "<div class='msg'><b><font color="+d[i].color+">" + d[i].user + ": </font></b>" + String(d[i].msg) + "</div>";
+      document.getElementById("globalmsgs").innerHTML += "<div class='msg'><b><font color="+d[i].color+">" + jsEscape(String(d[i].user)) + ": </font></b>" + jsEscape(String(d[i].msg)) + "</div>";
     }
   }
   })
 }
 
 setInterval(repeat, 3000);
+
+function jsEscape(str){
+  return String(str).replace(/[^\w. ]/gi, function(c){
+     return '\\u'+('0000'+c.charCodeAt(0).toString(16)).slice(-4);
+  });
+
+}
