a46a54d9e8a845f9524bc339bdf927a5479920c9
Your Name <you@example.com>
2022-01-13 01:20:59 +0530

    commit main

--- static/js/script.js
+++ static/js/script.js
@@ -2,24 +2,29 @@ var tmp = 0;
 
 function repeat() {
   fetch("http://localhost:8080/u/msglist").then(res => res.json()).then(d => {
-    
-  if(d.length > tmp){
-    tmp = d.length;
 
-    document.getElementById("globalmsgs").innerText = "";
-    
-    for (let i = 0; i < d.length; i++) {
-      document.getElementById("globalmsgs").innerHTML += "<div class='msg'><b><font color="+d[i].color+">" + jsEscape(String(d[i].user)) + ": </font></b>" + jsEscape(String(d[i].msg)) + "</div>";
+    if (d.length > tmp) {
+      tmp = d.length;
+
+      document.getElementById("globalmsgs").innerText = "";
+
+      for (let i = 0; i < d.length; i++) {
+        document.getElementById("globalmsgs").innerHTML += "<div class='msg'><b><font color=" + d[i].color + ">" + jsEscape(String(d[i].user)) + ": </font></b>" + jsEscape(String(d[i].msg)) + "</div>";
+      }
     }
-  }
-  })
+  });
 }
 
-setInterval(repeat, 3000);
+tmpinterval = setInterval(repeat, 3000);
 
-function jsEscape(str){
-  return String(str).replace(/[^\w. ]/gi, function(c){
-     return '\\u'+('0000'+c.charCodeAt(0).toString(16)).slice(-4);
+function lol(){
+  clearInterval(tmpinterval);
+  tmpinterval = setInterval(repeat, 3000);
+}
+
+function jsEscape(str) {
+  return String(str).replace(/[^\w. ]/gi, function (c) {
+    return '\\u' + ('0000' + c.charCodeAt(0).toString(16)).slice(-4);
   });
 
 }
