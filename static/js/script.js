2772864bb77358904858e1c466187f273965e143
Your Name <you@example.com>
2022-01-13 12:29:34 +0530

    commit main

--- static/js/script.js
+++ static/js/script.js
@@ -4,7 +4,6 @@ var lt = /</g,
     gt = />/g, 
     ap = /'/g, 
     ic = /"/g;
-value = value.toString().replace(lt, "&lt;").replace(gt, "&gt;").replace(ap, "&#39;").replace(ic, "&#34;");
 
 function repeat() {
   fetch("http://localhost:8080/u/msglist").then(res => res.json()).then(d => {
@@ -15,7 +14,9 @@ function repeat() {
       document.getElementById("globalmsgs").innerText = "";
 
       for (let i = 0; i < d.length; i++) {
-        document.getElementById("globalmsgs").innerHTML += "<div class='msg'><b><font color=" + d[i].color + ">" + jsEscape(String(d[i].user)) + ": </font></b>" + jsEscape(String(d[i].msg)) + "</div>";
+        d[i].msg = d[i].msg.toString().replace(lt, "&lt;").replace(gt, "&gt;").replace(ap, "&#39;").replace(ic, "&#34;");
+        d[i].usr = d[i].usr.toString().replace(lt, "&lt;").replace(gt, "&gt;").replace(ap, "&#39;").replace(ic, "&#34;");
+        document.getElementById("globalmsgs").innerHTML += "<div class='msg'><b><font color=" + d[i].color + ">" + d[i].user + ": </font></b>" + d[i].msg + "</div>";
       }
     }
   });
