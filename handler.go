e17f7106bc7077765adef39dfc92db07a2a09f0a
Your Name <you@example.com>
2022-01-13 10:55:04 +0530

    commit main

--- handler.go
+++ handler.go
@@ -93,7 +93,7 @@ func postmsg(c *gin.Context) {
 	umessage := c.PostForm("usermessage")
 
 	if strings.Contains(umessage, "<") || strings.Contains(umessage, ">") || strings.Contains(umessage, "[") || strings.Contains(umessage, "]") || strings.Contains(umessage, "\\") || strings.Contains(umessage, "/") || strings.Contains(umessage, ":") || strings.Contains(umessage, ")") || strings.Contains(umessage, "(") {
-		umessage = "Nice try..."
+		umessage = "Nice try."
 	}
 
 	data := message{fmt.Sprint(user), umessage, fmt.Sprint(color)}
